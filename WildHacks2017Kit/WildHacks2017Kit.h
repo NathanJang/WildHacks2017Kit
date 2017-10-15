559f7f4e22547e5e8748502203dd4989e513b7a4
Jonathan Chan <jonchanyc@outlook.com>
2017-10-15 12:12:25 -0500

    Initial commit

---
+++ WildHacks2017Kit/WildHacks2017Kit.h
@@ -0,0 +1,19 @@
+//
+//  WildHacks2017Kit.h
+//  WildHacks2017Kit
+//
+//  Created by Jonathan Chan on 2017-10-15.
+//  Copyright © 2017 Jonathan Chan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WildHacks2017Kit.
+FOUNDATION_EXPORT double WildHacks2017KitVersionNumber;
+
+//! Project version string for WildHacks2017Kit.
+FOUNDATION_EXPORT const unsigned char WildHacks2017KitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WildHacks2017Kit/PublicHeader.h>
+
+
